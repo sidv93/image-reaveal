4a48e24a12bdfe157d63cc04dcd455ed17a6a1c9
Siddharth Venkatesh <32375112+sidv93@users.noreply.github.com>
2020-07-11 21:55:44 +0530

    finished animation

--- src/Frame.js
+++ src/Frame.js
@@ -3,6 +3,8 @@ import styled from 'styled-components';
 import { motion } from 'framer-motion';
 import People from './assets/people.webp';
 
+const transition = { duration: 1.4, ease: [0.6, 0.01, -0.05, 0.9] };
+
 const Container = styled.div`
     height: 100vh;
     width: 100vw;
@@ -15,29 +17,45 @@ const FrameContainer = styled.div`
     height: 60%;
     width: 60%;
     position: relative;
+    overflow: hidden;
 `;
 
-const Image = styled.img`
+const Image = styled(motion.img)`
     width: 100%;
     height: 100%;
-    object-fit: contain;
+    object-fit: cover;
 `;
 
-const Overlay = styled.div`
+const Overlay = styled(motion.div)`
     position: absolute;
     top: 0;
-    left: 0;
+    right: 0;
     height: 100%;
-    width: 100%;
-    background-color: black;
+    background-color: #FFF;
 `;
 
 const Frame = () => {
     return (
         <Container>
             <FrameContainer>
-                <Image src={People} alt="people image" />
-                {/* <Overlay /> */}
+                <Image src={People} alt="people image"
+                    initial={{
+                        scale: 1.6
+                    }}
+                    animate={{
+                        scale: 1,
+                        transition
+                    }}
+                />
+                <Overlay
+                    initial={{
+                        width: '100%'
+                    }}
+                    animate={{
+                        width: '0',
+                        transition
+                    }}
+                />
             </FrameContainer>
         </Container>
     );
